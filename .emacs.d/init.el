10fb0ddf720816c624c40218da03e4024bbdcd82
snackon <48731449+snackon@users.noreply.github.com>
2019-04-03 00:12:21 -0300

    Update init.el

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -1,6 +1,4 @@
-
-;;makes emacs startup faster
- 
+;; Make emacs startup faster
 (setq gc-cons-threshold 402653184
       gc-cons-percentage 0.6)
  
@@ -16,43 +14,68 @@
  
 (add-hook 'emacs-startup-hook 'startup/revert-file-name-handler-alist)
 (add-hook 'emacs-startup-hook 'startup/reset-gc)
- 
 ;;
 
+;; Initialize melpa repo
 (require 'package)
 (setq package-enable-at-startup nil)
 (add-to-list 'package-archives
          '("melpa" . "https://melpa.org/packages/"))
 (package-initialize)
- 
+;;
+
+;; Initialize use-package
 (unless (package-installed-p 'use-package)
   (package-refresh-contents)
   (package-install 'use-package))
- 
+;;
+
+;; Initialize monokai theme
 (unless (package-installed-p 'monokai-theme)
   (package-refresh-contents)
   (package-install 'monokai-theme))
- 
-(setq make-backup-file nil)
+;;
+
+;; Disable automatic creation of backup files
+(setq make-backup-files nil)
 (setq auto-save-default nil)
+;;
+
+;; Make scrolling not so aggressive
 (setq scroll-conservatively 100)
- 
+;;
+
+;; Disable the heart-attack-inducing ringbell
 (setq ring-bell-function 'ignore)
- 
+;;
+
+;; Set the standard indent size to 4 spaces
 (setq-default standard-indent 4)
+; Set the default tab width to 4 spaces
 (setq-default tab-width 4)
+; Set C mode to use the default tab width
 (setq c-basic-offset tab-width)
+;;
 
+;; Set the monokai background to a easy-on-the-eyes dark color
 (setq monokai-background "#262626")
+;;
 
+;; Typing "lambda" makes the lambda letter to appear instead
 (global-prettify-symbols-mode )
+;;
 
+;; This makes the line in dashboard be complete instead of finish abruptly at half (You might not need this
 (set-fontset-font "fontset-default"
                   (cons page-break-lines-char page-break-lines-char)
                   (face-attribute 'default :family))
+;;
 
+;; Use-package section
+; Initialize spaceline
 (use-package spaceline
   :ensure t)
+; Initialize powerline and utilize the spaceline theme
 
 (use-package powerline
   :ensure t
@@ -60,36 +83,42 @@
   (spaceline-spacemacs-theme))
 (add-hook 'after-init-hook 'powerline-reset)
 
+; Initialize dashboard
 (use-package dashboard
   :ensure t
   :config
   (dashboard-setup-startup-hook)
   (setq dashboard-items '((recents . 5)))
   (setq dashboard-banner-logo-title "W I T C H M A C S")
-  (setq dashboard-startup-banner "/path/to/marisa.png")
+  (setq dashboard-startup-banner "/path/to/marisa.png") ; Remember to switch this to the path to your Marisa picture
   (setq dashboard-center-content t)
   (setq dashboard-show-shortcuts nil))
 
+; Initialize which-key
 (use-package which-key
   :ensure t
   :init
   (which-key-mode))
- 
+
+; Initialize evil mode
 (use-package evil
   :ensure t
   :init
   (evil-mode 1))
- 
+
+; Initialize bacon
 (use-package beacon
   :ensure t
   :init
   (beacon-mode 1))
- 
+
+; Initialize avy 
 (use-package avy
   :ensure t
   :bind
   ("M-s" . avy-goto-char))
- 
+
+; Initialize switch-window
 (use-package switch-window
   :ensure t
   :config
@@ -101,7 +130,8 @@
         '("a" "s" "d" "f" "j" "k" "l"))
   :bind
   ([remap other-window] . switch-window))
- 
+
+; Initialize yasnippet and snippets for C and C++ mode
 (use-package yasnippet
   :ensure t
   :config
@@ -145,7 +175,10 @@
   (add-hook 'c++-mode-hook 'irony-mode)
   (add-hook 'c-mode-hook 'irony-mode)
   (add-hook 'irony-mode-hook 'irony-cdb-autosetup-compile-options))
- 
+;
+;;
+
+;; Make brackets have matching pairs
 (setq electric-pair-pairs '(
                            (?\{ . ?\})
                            (?\( . ?\))
@@ -153,15 +186,25 @@
                            (?\" . ?\")
                           ))
 (electric-pair-mode t)
- 
-(setq x-select-enable-clipboard t)
- 
-(setq inhibit-startup-message t)
-(global-linum-mode t)
+ ;;
 
+;; Ctrl + v outside Emacs lets you paste it on Emacs with p and vice versa
+(setq x-select-enable-clipboard t)
+;;
+
+;; Disable the default startup screen
+(setq inhibit-startup-message t)
+;;
+
+;; Enable line numbers on the left
+(global-linum-mode t)
+;;
+
+;; Disable most gui elements
 (tool-bar-mode -1)
 (menu-bar-mode -1)
 (scroll-bar-mode -1)
+;;
 
 ;;powerline enables both of these by default, but if you don't want to use powerline you can uncomment these
 ;;(line-number-mode 1)
