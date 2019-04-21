dac03b9414027eafaba1c4637ad9cf9f97e70de6
Snackon <Snacks@disroot.org>
2019-04-21 17:43:45 -0400

    woops haha

--- init.el
+++ init.el
@@ -206,17 +206,3 @@
   (add-hook 'c-mode-hook 'irony-mode)
   (add-hook 'irony-mode-hook 'irony-cdb-autosetup-compile-options))
 ;;
-(custom-set-variables
- ;; custom-set-variables was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(package-selected-packages
-   (quote
-	(company-irony company-c-headers company yasnippet-snippets yasnippet switch-window avy beacon evil which-key dashboard spaceline use-package))))
-(custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- )
